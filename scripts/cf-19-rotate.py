0e0e4ef45982ca8821b13de34c685d268839584a
foundationkitty <45774850+foundationkitty@users.noreply.github.com>
2026-06-11 00:49:18 -0700

    wayland screen rotate

--- scripts/cf-19-rotate.py
+++ scripts/cf-19-rotate.py
@@ -1,27 +1,26 @@
 import subprocess
 import os
 
-normal = '1 0 0 0 1 0 0 0 1'
-inverted = '-1 0 1 0 -1 1 0 0 1'
-right = '0 1 0 -1 0 1 0 0 1'
-left = '0 -1 1 1 0 0 0 0 1'
+normal = '"1 0 0 0 1 0"'
+inverted = '" -1 0 1 0 -1 1"'
+right = '"0 1 0 -1 0 1"'
+left = '"0 -1 1 1 0 0"'
 
-devname = '"MosArt MosArt 10.1"'
-prop_cmd = "xinput set-prop " + devname + ' "Coordinate Transformation Matrix" '
+dispname = "LVDS-1"
+prop_cmd = "swaymsg input '*' calibration_matrix "
 
-query_cmd = "xrandr -q"
+query_cmd = "swaymsg -t get_outputs"
 query = subprocess.check_output(query_cmd, shell=True, text=True)
 
-trim = query.split(" (")[0]
-if "right" in trim:
-  os.system("xrandr -o inverted")
-  os.system(prop_cmd + inverted)
-elif "inverted" in trim:
-  os.system("xrandr -o left")
-  os.system(prop_cmd + left)
-elif "left" in trim:
-  os.system("xrandr -o normal")
-  os.system(prop_cmd + normal)
-else:
-  os.system("xrandr -o right")
+if '"transform": "normal"' in query:
+  os.system("swaymsg output " + dispname + " transform 90 ;")
   os.system(prop_cmd + right)
+elif '"transform": "90"' in query:
+  os.system("swaymsg output " + dispname + " transform 180 ;")
+  os.system(prop_cmd + inverted)
+elif '"transform": "180"' in query:
+  os.system("swaymsg output " + dispname + " transform 270 ;")
+  os.system(prop_cmd + left)
+else:
+  os.system("swaymsg output " + dispname + " transform 0 ;")
+  os.system(prop_cmd + normal)
